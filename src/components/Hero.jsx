2041f56a91b3828f8c9cfb3716b5e2278ab3924e
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-22 22:02:28 -0400

    Component Section Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import Search from "./Search";
+
+const Hero = () => {
+    return (
+        <div className="background flex flex-col justify-center items-center">
+            <Search />
+        </div>
+    );
+};
+
+export default Hero;
