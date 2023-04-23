7862e7d5451b14d9b74b967e622051cce94c7a00
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-23 14:19:13 -0400

    Use component ListDrinks

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { CategoryProvider } from "./context/CategoryProvider";
 import { DrinksProvider } from "./context/DrinksProvider";
 import { ToastContainer } from "react-toastify";
 import Hero from "./components/Hero";
+import ListDrinks from "./components/ListDrinks";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <DrinksProvider>
                 <ToastContainer theme="dark" />
                 <Hero />
+                <ListDrinks />
             </DrinksProvider>
         </CategoryProvider>
     );
