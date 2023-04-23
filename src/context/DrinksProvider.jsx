be692c8c168d3de45503df7da823009e80857d1b
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-23 00:53:08 -0400

    Context Drinks

---
+++ src/context/DrinksProvider.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+
+const DrinksContext = createContext();
+
+export const DrinksProvider = ({ children }) => {
+    return (
+        <DrinksContext.Provider value={{}}>{children}</DrinksContext.Provider>
+    );
+};
+
+export default DrinksContext;
