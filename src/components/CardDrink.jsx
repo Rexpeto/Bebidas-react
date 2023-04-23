20849d88f06cefe7fe33c532957b080c17b1f689
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-23 16:09:19 -0400

    Open modal

--- src/components/CardDrink.jsx
+++ src/components/CardDrink.jsx
@@ -1,20 +1,27 @@
+import useDrinks from "../hooks/useDrinks";
+
 const CardDrink = ({ drink }) => {
-    const { strDrink, strDrinkThumb } = drink;
+    const { handdlerModal } = useDrinks();
+    const { strDrink, strDrinkThumb, idDrink } = drink;
     return (
-        <div class="max-w-sm border bg-gray-800/50 backdrop-blur-xl rounded-lg shadow border-gray-700">
-            <a href="#">
-                <img class="rounded-t-lg" src={strDrinkThumb} alt={strDrink} />
-            </a>
-            <div class="p-5">
-                <h5 class="mb-4 text-2xl font-bold tracking-tight text-white">
+        <div className="max-w-sm border bg-gray-800/50 backdrop-blur-xl rounded-lg shadow border-gray-700">
+            <button onClick={() => handdlerModal(idDrink)}>
+                <img
+                    className="rounded-t-lg"
+                    src={strDrinkThumb}
+                    alt={strDrink}
+                />
+            </button>
+            <div className="p-5">
+                <h5 className="mb-4 text-2xl font-bold tracking-tight text-white">
                     {strDrink}
                 </h5>
-                <a
-                    href="#"
-                    class="block items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg outline-none bg-blue-600 hover:bg-blue-700 transition duration-150"
+                <button
+                    onClick={() => handdlerModal(idDrink)}
+                    className="block w-full items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg outline-none bg-blue-600 hover:bg-blue-700 transition duration-150"
                 >
                     Saber más
-                </a>
+                </button>
             </div>
         </div>
     );
