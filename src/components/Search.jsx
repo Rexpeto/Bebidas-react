dc2852c91a0eadbec03da91fd3269671806ccd6b
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-22 23:43:30 -0400

    New layout

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,15 +2,16 @@ import { RiSearchLine } from "react-icons/ri";
 
 const Search = () => {
     return (
-        <form className="flex gap-2">
+        <form className="flex gap-2 w-full justify-center">
             <select className=" border text-sm rounded-lg block px-3 bg-gray-800/50 backdrop-blur-xl border-gray-600 placeholder-gray-400 text-white focus:border-blue-500 outline-none transition duration-150">
                 <option value="">Categorías</option>
             </select>
             <div className="relative w-[30rem]">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-[2]">
-                    <RiSearchLine className="text-white" />
+                    <RiSearchLine className="text-white" htmlFor="search" />
                 </div>
                 <input
+                    id="search"
                     type="text"
                     className="block w-full p-4 pl-10 text-sm bg-gray-800/50 backdrop-blur-xl border-gray-800 border rounded-lg focus:border-blue-500 placeholder-gray-400 text-white outline-none transition duration-150"
                     placeholder="Buscar Bebida"
