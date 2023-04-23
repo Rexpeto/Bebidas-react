9be0a2722b9f467994ec96ede253cad61d54014f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-23 00:32:05 -0400

    Add Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { CategoryProvider } from "./context/CategoryProvider";
+import { ToastContainer } from "react-toastify";
 import Hero from "./components/Hero";
 
 const App = () => {
     return (
         <CategoryProvider>
+            <ToastContainer theme="dark" />
             <Hero />
         </CategoryProvider>
     );
