b4404bc3b368d50da3997cef860a1585a26c10c4
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-23 01:13:19 -0400

    Get Drinks api

--- src/context/DrinksProvider.jsx
+++ src/context/DrinksProvider.jsx
@@ -1,11 +1,37 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import axios from "axios";
 
 const DrinksContext = createContext();
 
 export const DrinksProvider = ({ children }) => {
+    //* State drink
+    const [result, setResult] = useState([]);
+
+    //* Get drinks Api
+    const getDrinks = async (obj) => {
+        try {
+            const {
+                data: { drinks },
+            } = await axios(
+                `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${obj?.name}&c=${obj?.category}`
+            );
+
+            setResult(drinks);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <DrinksContext.Provider value={{}}>{children}</DrinksContext.Provider>
+        <DrinksContext.Provider
+            value={{
+                getDrinks,
+                result,
+            }}
+        >
+            {children}
+        </DrinksContext.Provider>
     );
 };
 
